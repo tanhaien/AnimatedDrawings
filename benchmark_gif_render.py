5243e5f58f8c20a8ee0555fa09e0e5a6c31ed7c8
tanhaien <tuananh.ai.en@gmail.com>
2024-07-31 21:12:26 +0700

    optimized by PyOpenGL accelerate

--- benchmark_gif_render.py
+++ benchmark_gif_render.py
@@ -20,10 +20,10 @@ def benchmark_gif_render():
     # In thông số tổng quát
     print(f"Tổng thời gian render: {end_time - start_time:.2f} giây")
     
-    # Xử lý và in kết quả chi tiết từ cProfile
+#     Xử lý và in kết quả chi tiết từ cProfile
     s = io.StringIO()
     ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-    ps.print_stats(20)  # In top 20 hàm tốn nhiều thời gian nhất
+    ps.print_stats(100)  # In top 20 hàm tốn nhiều thời gian nhất
     print(s.getvalue())
 
 if __name__ == "__main__":
