91174cd6632c4df25bd3d8d889e369065c7d0959
tanhaien <tuananh.ai.en@gmail.com>
2024-08-07 00:06:58 +0700

    replace BFS by KDTree for 3D mesh searching

--- benchmark_gif_render.py
+++ benchmark_gif_render.py
@@ -21,10 +21,10 @@ def benchmark_gif_render():
     print(f"Tổng thời gian render: {end_time - start_time:.2f} giây")
     
 # #     Xử lý và in kết quả chi tiết từ cProfile
-#     s = io.StringIO()
-#     ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
-#     ps.print_stats(100)  # In top 20 hàm tốn nhiều thời gian nhất
-#     print(s.getvalue())
+    # s = io.StringIO()
+    # ps = pstats.Stats(pr, stream=s).sort_stats('cumulative')
+    # ps.print_stats(1000)  # In top 20 hàm tốn nhiều thời gian nhất
+    # print(s.getvalue())
 
 if __name__ == "__main__":
     benchmark_gif_render()
