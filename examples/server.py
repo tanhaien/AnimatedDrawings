9d8da0b9ff97813d6cfd3c90b93a38e9bd9e5b2f
tanhaien <tuananh.ai.en@gmail.com>
2024-08-23 12:27:08 +0700

    stable version

--- examples/server.py
+++ examples/server.py
@@ -10,6 +10,8 @@ import cv2
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 import threading
+import yaml
+from pkg_resources import resource_filename
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Giới hạn kích thước tải lên 16MB
@@ -31,31 +33,52 @@ def get_file_hash(file):
     file.seek(0)  # Đặt lại con trỏ file
     return hasher.hexdigest()
 
-def resize_image(image, size=(300, 300)):
+#def resize_image(image, size=(300, 300)):
+#    # Đọc ảnh từ file
+#    img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+#    image.seek(0)  # Đặt lại con trỏ file
+    # 
+    # # Resize ảnh
+    # resized = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+    # 
+    # # Chuyển đổi ảnh thành bytes
+    # _, img_encoded = cv2.imencode('.png', resized)
+    # return img_encoded.tobytes()
+
+def resize_image(image, max_size=300):
     # Đọc ảnh từ file
     img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
     image.seek(0)  # Đặt lại con trỏ file
     
+    # Tính toán kích thước mới giữ nguyên tỷ lệ
+    height, width, _ = img.shape
+    if max(height, width) <= max_size:
+        return img_encoded.tobytes()
+    
+    if height > width:
+        new_height = max_size
+        new_width = int(width * (max_size / height))
+    else:
+        new_width = max_size
+        new_height = int(height * (max_size / width))
+    
     # Resize ảnh
-    resized = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+    resized = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_AREA)
     
     # Chuyển đổi ảnh thành bytes
     _, img_encoded = cv2.imencode('.png', resized)
     return img_encoded.tobytes()
+    
+def get_available_motions():
+    motion_dir = resource_filename(__name__, 'config/motion')
+    motions = [f for f in os.listdir(motion_dir) if f.endswith('.yaml')]
+    motions.sort()
+    return motions
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-def process_animation(annotations_dir):
-    motion_cfg_fn = 'config/motion/dab.yaml'
-    retarget_cfg_fn = 'config/retarget/fair1_ppf.yaml'
-    
-    start_time = time.time()
-    annotations_to_animation(annotations_dir, motion_cfg_fn, retarget_cfg_fn)
-    end_time = time.time()
-    
-    return end_time - start_time
+    motions = get_available_motions()
+    return render_template('index.html', motions=motions)
 
 @app.route('/render_gif', methods=['POST'])
 def render_gif():
@@ -63,6 +86,10 @@ def render_gif():
         return 'Không có file ảnh nào được gửi', 400
     
     image = request.files['image']
+    motion = request.form.get('motion', 'dab.yaml')
+    
+    motion = os.path.join('config/motion', motion)
+    
     resized_image = resize_image(image)
     file_hash = hashlib.md5(resized_image).hexdigest()
     
@@ -79,15 +106,24 @@ def render_gif():
             image_to_annotations(str(image_path), str(annotations_dir))
     
     # Sử dụng thread pool để xử lý tác vụ nặng
-    future = executor.submit(process_animation, str(annotations_dir))
+    future = executor.submit(process_animation, str(annotations_dir), motion)
     
     # Đợi kết quả từ thread pool
     render_time = future.result()
     
     return jsonify({
         'render_time': f'{render_time:.2f}',
-        'gif_url': f'/get_gif/{file_hash}'
+        'gif_url': f'/get_gif/{file_hash}?t={int(time.time())}'  # Thêm timestamp
     })
+
+def process_animation(annotations_dir, motion_cfg_fn):
+    retarget_cfg_fn = 'config/retarget/fair1_ppf.yaml'
+    
+    start_time = time.time()
+    annotations_to_animation(annotations_dir, motion_cfg_fn, retarget_cfg_fn)
+    end_time = time.time()
+    
+    return end_time - start_time
 
 @app.route('/get_gif/<path:file_hash>')
 def get_gif(file_hash):
